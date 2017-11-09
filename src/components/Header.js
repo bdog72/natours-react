948d14c29f1e5bf912f6f9f2e7d04dfb13b2f967
Brian Bycynski <high5bri@hotmail.com>
2017-11-09 06:27:11 -0500

    finished lecture 8

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render () {
+    return (
+      <div>
+        <header className='header'>
+          <div className='logo-box'>
+            <img src={require('../../public/images/logo-white.png')} alt='logo' className='logo' />
+          </div>
+          <div className='text-box'>
+            <h1 className='heading-primary'>
+              <span>App-App-D</span>
+              <span className='heading-primary-main'>Outdoors</span>
+              <span className='heading-primary-sub'>is where life happens</span>
+            </h1>
+          </div>
+        </header>
+      </div>
+    )
+  }
+}
