f633eda5df787c4e536dbd62900e9b160f59cf84
Brian Bycynski <high5bri@hotmail.com>
2017-11-09 11:06:10 -0500

    finished section 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,16 @@ export default class Header extends Component {
     return (
       <div>
         <header className='header'>
-          <div className='logo-box'>
-            <img src={require('../../public/images/logo-white.png')} alt='logo' className='logo' />
+          <div className='header__logo-box'>
+            <img src={require('../../public/images/logo-white.png')} alt='logo' className='header__logo' />
           </div>
-          <div className='text-box'>
+          <div className='header__text-box'>
             <h1 className='heading-primary'>
               <span>App-App-D</span>
-              <span className='heading-primary-main'>Outdoors</span>
-              <span className='heading-primary-sub'>is where life happens</span>
+              <span className='heading-primary--main'>Outdoors</span>
+              <span className='heading-primary--sub'>is where life happens</span>
             </h1>
-            <a href='#' className='btn btn-white btn-animated'>Discover Our Tours</a>
+            <a href='#' className='btn btn--white btn--animated'>Discover Our Tours</a>
           </div>
         </header>
       </div>
