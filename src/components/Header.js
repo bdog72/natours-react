9bae769bdd50744580e9e1c326596150e35b6f95
Brian Bycynski <high5bri@hotmail.com>
2017-11-10 08:06:30 -0500

    still working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Header extends Component {
-  render () {
-    return (
-      <div>
-        <header className='header'>
-          <div className='header__logo-box'>
-            <img src={require('../../public/images/logo-white.png')} alt='logo' className='header__logo' />
-          </div>
-          <div className='header__text-box'>
-            <h1 className='heading-primary'>
-              <span>App-App-D</span>
-              <span className='heading-primary--main'>Outdoors</span>
-              <span className='heading-primary--sub'>is where life happens</span>
-            </h1>
-            <a href='#' className='btn btn--white btn--animated'>Discover Our Tours</a>
-          </div>
-        </header>
+const Header = () => (
+  <div>
+    <header className='header'>
+      <div className='header__logo-box'>
+        <img src={require('../../public/images/logo-white.png')} alt='logo' className='header__logo' />
       </div>
-    )
-  }
-}
+      <div className='header__text-box'>
+        <h1 className='heading-primary'>
+          <span>App-App-D</span>
+          <span className='heading-primary--main'>Outdoors</span>
+          <span className='heading-primary--sub'>is where life happens</span>
+        </h1>
+        <a href='#' className='btn btn--white btn--animated'>Discover Our Tours</a>
+      </div>
+    </header>
+  </div>
+)
+export default Header
